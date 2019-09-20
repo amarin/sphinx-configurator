f360fde7c19fdf48644a660effef221734e23c06
Marin Aleksey <asmadews@gmail.com>
2019-09-20 16:17:39 +0300

    allow replace default template with custom

--- src/sphinx_configurator/rewritable.py
+++ src/sphinx_configurator/rewritable.py
@@ -6,15 +6,22 @@ from sphinx_configurator.constants import NOTSET
 DEFAULT_PATH = path.join(path.dirname(__file__), 'defaults')
 
 
-def rewritable_file_path(app, filename, default_path=NOTSET):
+def rewritable_file_path(app, filename, local_path=NOTSET, default_path=NOTSET):
     """Get filepath in documentation src or default in sphinx_configurator"""
     src_dir = app.srcdir
-    found_path = path.join(src_dir, filename)
+    if local_path:
+        found_path = path.join(src_dir, local_path, filename)
+    else:
+        found_path = path.join(src_dir, filename)
+
     if path.exists(found_path):
         return found_path
 
     if default_path is NOTSET:
         default_path = DEFAULT_PATH
+    elif default_path is None:
+        raise FileNotFoundError(f"Local file {found_path} not found")
+
     return path.join(default_path, filename)
 
 
